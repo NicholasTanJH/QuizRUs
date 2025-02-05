9cfaf703221fc00a1ac6f839003daefdc88b5907
Nicholas <warsnwars@gmail.com>
2025-02-05 16:07:33 -0600

    add the submit and next button for the mcquestion page

--- app/src/main/java/comp3350/quizrus/presentation/MCQuestionActivity.java
+++ app/src/main/java/comp3350/quizrus/presentation/MCQuestionActivity.java
@@ -3,6 +3,7 @@ package comp3350.quizrus.presentation;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 import comp3350.quizrus.R;
 
 public class MCQuestionActivity extends AppCompatActivity {
-    private Button option1;
-    private Button option2;
-    private Button option3;
-    private Button option4;
+    private Button optionButton1;
+    private Button optionButton2;
+    private Button optionButton3;
+    private Button optionButton4;
+    //keep track of which button is last pressed before clicking submit button
     private int lastPressedButtonId = -1;
+    private Button proceedButton;
+    private boolean isSubmitted = false;
 
 
     @Override
@@ -32,7 +36,8 @@ public class MCQuestionActivity extends AppCompatActivity {
         });
 
         setQuestionText("Question: How cold is today?");
-        initButton();
+        setUpOptionButtons();
+        setUpProceedButton();
     }
 
     //set the question text
@@ -41,52 +46,118 @@ public class MCQuestionActivity extends AppCompatActivity {
         questionTv.setText(questionText);
     }
 
+    /*
+    initialize the option buttons
+    add onclick listener that:
+        track the last pressed button id
+        change the option button color when pressed
+        make the proceed button into submit mode once any of the option button is pressed
+     */
+    private void setUpOptionButtons() {
+        optionButton1 = findViewById(R.id.button1);
+        optionButton2 = findViewById(R.id.button2);
+        optionButton3 = findViewById(R.id.button3);
+        optionButton4 = findViewById(R.id.button4);
+
+        optionButton1.setOnClickListener(b -> {
+            if(!isSubmitted)
+                setProceedButtonToSubmitMode();
+            lastPressedButtonId = b.getId();
+            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
+            optionButton2.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton3.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton4.setBackgroundResource(R.drawable.question_option_button_default_color);
+        });
+
+        optionButton2.setOnClickListener(b -> {
+            if(!isSubmitted)
+                setProceedButtonToSubmitMode();
+            lastPressedButtonId = b.getId();
+            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
+            optionButton1.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton3.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton4.setBackgroundResource(R.drawable.question_option_button_default_color);
+        });
+
+        optionButton3.setOnClickListener(b -> {
+            if(!isSubmitted)
+                setProceedButtonToSubmitMode();
+            lastPressedButtonId = b.getId();
+            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
+            optionButton1.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton2.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton4.setBackgroundResource(R.drawable.question_option_button_default_color);
+        });
+
+        optionButton4.setOnClickListener(b -> {
+            if(!isSubmitted)
+                setProceedButtonToSubmitMode();
+            lastPressedButtonId = b.getId();
+            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
+            optionButton1.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton2.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton3.setBackgroundResource(R.drawable.question_option_button_default_color);
+        });
+    }
+
     //set the option button text
-    private void setOptionsText(String optionText, Button button){
-           button.setText(optionText);
+    private void setOptionsText(String optionText, Button button) {
+        button.setText(optionText);
     }
 
     /*
-    initialize the button
-    add onclick listener that track the last pressed button id
-    and change color when pressed
+    initialize the proceedButton by finding the id
+    make it into invisible mode
+    **proceed button is either in invisible mode, submit, or next mode**
      */
-    private void initButton() {
-        option1 = findViewById(R.id.button1);
-        option2 = findViewById(R.id.button2);
-        option3 = findViewById(R.id.button3);
-        option4 = findViewById(R.id.button4);
+    private void setUpProceedButton() {
+        proceedButton = findViewById(R.id.buttonProceed);
+        setProceedButtonToInvisibleMode();
+    }
 
-        option1.setOnClickListener(b -> {
-            lastPressedButtonId = b.getId();
-            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
-            option2.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option3.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option4.setBackgroundResource(R.drawable.question_option_button_default_color);
+    //set the proceed button to invisible and unclickable
+    private void setProceedButtonToInvisibleMode() {
+        proceedButton.setVisibility(View.INVISIBLE);
+        proceedButton.setEnabled(false);
+    }
+
+    //make the proceed button visible and usable
+    //make it as a submit button
+    private void setProceedButtonToSubmitMode() {
+        isSubmitted = true;
+        proceedButton.setText(R.string.submit);
+        proceedButton.setVisibility(View.VISIBLE);
+        proceedButton.setEnabled(true);
+        proceedButton.setBackgroundResource(R.drawable.question_proceed_button_submit);
+        proceedButton.setOnClickListener(proceedButton -> {
+            //set the proceed button from the submit mode to next mode
+            setProceedButtonToNextMode();
+            //set the answer option buttons to unusable
+            optionButton1.setClickable(false);
+            optionButton2.setClickable(false);
+            optionButton3.setClickable(false);
+            optionButton4.setClickable(false);
         });
+    }
 
-        option2.setOnClickListener(b -> {
-            lastPressedButtonId = b.getId();
-            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
-            option1.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option3.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option4.setBackgroundResource(R.drawable.question_option_button_default_color);
-        });
-
-        option3.setOnClickListener(b -> {
-            lastPressedButtonId = b.getId();
-            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
-            option1.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option2.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option4.setBackgroundResource(R.drawable.question_option_button_default_color);
-        });
-
-        option4.setOnClickListener(b -> {
-            lastPressedButtonId = b.getId();
-            b.setBackgroundResource(R.drawable.question_option_button_clicked_color);
-            option1.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option2.setBackgroundResource(R.drawable.question_option_button_default_color);
-            option3.setBackgroundResource(R.drawable.question_option_button_default_color);
+    //make it as a next button
+    private void setProceedButtonToNextMode() {
+        isSubmitted = false;
+        proceedButton.setText(R.string.next);
+        proceedButton.setBackgroundResource(R.drawable.question_proceed_button_next);
+        proceedButton.setOnClickListener(proceedButton -> {
+            //set the proceed button from next mode to invisible mode
+            setProceedButtonToInvisibleMode();
+            //clear the color of the selected option answer
+            optionButton1.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton2.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton3.setBackgroundResource(R.drawable.question_option_button_default_color);
+            optionButton4.setBackgroundResource(R.drawable.question_option_button_default_color);
+            //set the option button clickable
+            optionButton1.setClickable(true);
+            optionButton2.setClickable(true);
+            optionButton3.setClickable(true);
+            optionButton4.setClickable(true);
         });
     }
 }
