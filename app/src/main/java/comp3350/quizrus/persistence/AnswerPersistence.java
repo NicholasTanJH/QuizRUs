fbd7bf9647a6e0d82991d1c27e64d10963d13a7f
Tega Okene <okenetega@gmail.com>
2025-02-06 11:55:35 -0600

    Add indicator for correct or incorrect answers in persistence stub

--- app/src/main/java/comp3350/quizrus/persistence/AnswerPersistence.java
+++ app/src/main/java/comp3350/quizrus/persistence/AnswerPersistence.java
@@ -8,4 +8,6 @@ import comp3350.quizrus.objects.Question;
 public interface AnswerPersistence {
     List<Answer> getAnswersForQuestion(Question question);
 
+    List<Answer> getCorrectAnswers(Question question);
+
 }
