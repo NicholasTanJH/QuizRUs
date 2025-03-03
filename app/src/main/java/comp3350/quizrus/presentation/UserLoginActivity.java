9352c6f40d178da950086b1a93f367caac68938f
Tega Okene <okenetega@gmail.com>
2025-03-02 22:33:43 -0600

    Get the correct full path to init.sql within the internal storage.

--- app/src/main/java/comp3350/quizrus/presentation/UserLoginActivity.java
+++ app/src/main/java/comp3350/quizrus/presentation/UserLoginActivity.java
@@ -1,7 +1,9 @@
 package comp3350.quizrus.presentation;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +42,7 @@ public class UserLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        copyDatabaseToDevice();
+        instantiateDatabase();
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -110,7 +112,7 @@ public class UserLoginActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void copyDatabaseToDevice() {
+    private void instantiateDatabase() {
         final String DB_PATH = "db";
 
         String[] assetNames;
@@ -119,31 +121,46 @@ public class UserLoginActivity extends AppCompatActivity {
         AssetManager assetManager = getAssets();
 
         try {
-
             assetNames = assetManager.list(DB_PATH);
             for (int i = 0; i < assetNames.length; i++) {
                 assetNames[i] = DB_PATH + "/" + assetNames[i];
             }
 
-            List<String> paths = getAssetPaths(assetNames, dataDirectory);
+            copyAssetsToDirectory(assetNames, dataDirectory);
 
             Main.setDBPathName(dataDirectory.toString() + "/" + Main.getDBPathName());
-            DatabaseManager.executeSQLFromFile(paths.get(0));
+            DatabaseManager.executeSQLFromFile(dataDirectory + "/" + "init.sql");
 
         } catch (final IOException ioe) {
             System.err.println("Unable to access application data: " + ioe.getMessage());
         }
     }
 
-    public List<String> getAssetPaths(String[] assets, File directory) throws IOException {
-        List<String> paths = new ArrayList<>();
+    public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+        AssetManager assetManager = getAssets();
 
         for (String asset : assets) {
             String[] components = asset.split("/");
             String copyPath = directory.toString() + "/" + components[components.length - 1];
 
-            paths.add(copyPath);
+            char[] buffer = new char[1024];
+            int count;
+
+            File outFile = new File(copyPath);
+
+            if (!outFile.exists()) {
+                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+                FileWriter out = new FileWriter(outFile);
+
+                count = in.read(buffer);
+                while (count != -1) {
+                    out.write(buffer, 0, count);
+                    count = in.read(buffer);
+                }
+
+                out.close();
+                in.close();
+            }
         }
-        return paths;
     }
 }
