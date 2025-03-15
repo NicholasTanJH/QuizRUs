87a5cf7f7a91fd22a93a1bd78b0a06419cd2a0ac
Nicholas <tannjh@myumanitoba.ca>
2025-03-14 21:56:08 -0500

    add preview page and leaderboard with hard coded data;

---
+++ app/src/main/java/comp3350/quizrus/presentation/adapter/LeaderboardRecycleViewAdapter.java
@@ -0,0 +1,89 @@
+package comp3350.quizrus.presentation.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import comp3350.quizrus.R;
+
+public class LeaderboardRecycleViewAdapter extends RecyclerView.Adapter<LeaderboardRecycleViewAdapter.MyViewHolder> {
+    Context context;
+    String[] leaderboardNames;
+    int[] leaderboardScores;
+    boolean isEmptyLeaderboard;
+
+    public LeaderboardRecycleViewAdapter(Context context, String[] leaderboardNames, int[] leaderboardScore) {
+        this.context = context;
+        this.leaderboardNames = leaderboardNames;
+        this.leaderboardScores = leaderboardScore;
+        isEmptyLeaderboard = (leaderboardNames == null || leaderboardNames.length == 0);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_leaderboard_row, parent, false);
+        return new LeaderboardRecycleViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        TextView leaderboardOrderNumberTV = holder.leaderboardOrderNumberTV;
+        TextView leaderboardNameTV = holder.leaderboardNameTV;
+        TextView leaderboardScoreTV = holder.leaderboardScoreTV;
+
+        //show a default leaderboard item if the leaderboard is empty
+        if(isEmptyLeaderboard){
+            leaderboardNameTV.setText("This quiz hasn’t been attempted yet");
+        }else{
+            int currOrderNumber = position + 1;
+            String currName = leaderboardNames[position];
+            int currScore = leaderboardScores[position];
+
+            // Changing the name of the item according to the position
+            leaderboardOrderNumberTV.setText(currOrderNumber + ".");
+            leaderboardNameTV.setText(currName);
+            leaderboardScoreTV.setText(Integer.toString(currScore));
+
+            // Set background color based on order number
+            if(currOrderNumber == 1) {
+                holder.itemView.setBackgroundResource(R.drawable.leaderboard_first); // Gold background for 1st place
+            } else if(currOrderNumber == 2) {
+                holder.itemView.setBackgroundResource(R.drawable.leaderboard_second); // Silver background for 2nd place
+            } else if(currOrderNumber == 3) {
+                holder.itemView.setBackgroundResource(R.drawable.leaderboard_third); // Bronze background for 3rd place
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(isEmptyLeaderboard){
+            return 1;
+        }else{
+            return leaderboardNames.length;
+        }
+    }
+
+    // ==============================ViewHolder
+    // Class==========================================================
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView leaderboardOrderNumberTV;
+        TextView leaderboardNameTV;
+        TextView leaderboardScoreTV;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            leaderboardOrderNumberTV = itemView.findViewById(R.id.leaderboardOrderNumberTV);
+            leaderboardNameTV = itemView.findViewById(R.id.leaderboardNameTV);
+            leaderboardScoreTV = itemView.findViewById(R.id.leaderboardScoreTV);
+        }
+
+    }
+}
