2eb87e7d85d5c2460ed83cec8b357ffc5baf7f1e
Huzaifa Mehboob <mehboob1@myumanitoba.ca>
2025-03-27 23:54:00 -0500

    Added system test - SignupTest

---
+++ app/src/androidTest/java/comp3350/quizrus/SignupTest.java
@@ -0,0 +1,58 @@
+package comp3350.quizrus;
+import static android.os.SystemClock.sleep;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.quizrus.presentation.UserLoginActivity;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.anything;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SignupTest {
+    @Rule
+    public ActivityTestRule<UserLoginActivity> activityRule = new ActivityTestRule<>(UserLoginActivity.class);
+
+    @Test
+    public void signup() {
+        onView(withId(R.id.buttonSignUp)).perform(click());
+
+        // Fill in sign-up details
+        onView(withId(R.id.textInputETUsername)).perform(typeText("testUser"));
+        onView(withId(R.id.textInputETPassword)).perform(typeText("Password00!"));
+        onView(withId(R.id.textInputETConfirmPassword)).perform(typeText("Password00!"));
+        onView(withId(R.id.textInputETFirstName)).perform(typeText("John"));
+        onView(withId(R.id.textInputETLastName)).perform(typeText("Doe"));
+        closeSoftKeyboard();
+        // Click sign-up button
+        onView(withId(R.id.buttonCreateAccount)).perform(click());
+
+        //Test sign in
+        sleep(5000);
+        onView(withId(R.id.textInputETUsername)).perform(typeText("huzaifa"));
+        onView(withId(R.id.textInputETPassword)).perform(typeText("Quizrus69!"));
+        closeSoftKeyboard();
+
+        // Click sign-in button
+        onView(withId(R.id.buttonLogin)).perform(click());
+        sleep(5000);
+    }
+
+}
+
