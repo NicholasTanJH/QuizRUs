912d5fe560feafc19a56c9d3db56b49ee2dfa9f0
Nicholas <tannjh@myumanitoba.ca>
2025-03-28 13:16:24 -0500

    Add AllAcceptanceTests

---
+++ app/src/androidTest/java/comp3350/quizrus/AllAcceptanceTests.java
@@ -0,0 +1,19 @@
+package comp3350.quizrus;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AddQuizTest.class,
+        AttemptQuizTest.class,
+        DeleteQuizTest.class,
+        SearchQuizzesTest.class,
+        SignInOutTest.class,
+        SignupTest.class,
+})
+
+public class AllAcceptanceTests {
+}
+
+
